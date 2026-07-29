4ec2254c0c6052193534e257cd505c353b1fc319
Adnan <adnan.24150@gmail.com>
2026-07-29 10:34:11 +0200

    v23

--- app/Services/RakMadDeviceValService.php
+++ app/Services/RakMadDeviceValService.php
@@ -2,24 +2,32 @@
 
 namespace App\Services;
 
+use App\Exceptions\DBSaveException;
 use App\Models\RakMadDeviceVal;
+use Throwable;
 
 class RakMadDeviceValService
 {
+    /**
+     * @throws DBSaveException
+     */
     public function store(array $points, array $payload): void
     {
         foreach ($points as $point) {
-
-            RakMadDeviceVal::updateOrCreate(
-                [
-                    'LS_Nummer' => $payload['LS_Nummer'],
-                    'GeraeteNummer' => $payload['GeraeteNummer'],
-                    'Datum' => $point['date'],
-                ],
-                [
-                    'Wert' => $point['value'],
-                ]
-            );
+            try {
+                RakMadDeviceVal::updateOrCreate(
+                    [
+                        'LS_Nummer' => $payload['LS_Nummer'],
+                        'GeraeteNummer' => $payload['GeraeteNummer'],
+                        'Datum' => $point['date'],
+                    ],
+                    [
+                        'Wert' => $point['value'],
+                    ]
+                );
+            } catch (Throwable $e) {
+                throw new DBSaveException('Fehler beim Speichern oder Aktualisieren RakMadDeviceVal: ' . $e->getMessage());
+            }
         }
     }
 }
